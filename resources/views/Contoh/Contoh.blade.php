17b5ade21781e1b5e8c50c81d5ad07c98b098d4c
IPKIN9 <irwandirini@gmail.con>
2021-10-07 16:43:07 +0800

    Add tabs ini panel

--- resources/views/Contoh/Contoh.blade.php
+++ resources/views/Contoh/Contoh.blade.php
@@ -5,6 +5,31 @@
 
 <div class="content-wrapper">
     <div class="row">
+        <div class="col-12">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#table">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#form">Menu 1</a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div id="table" class="container tab-pane active"><br>
+                    <h3>HOME</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                </div>
+                <div id="form" class="container tab-pane fade"><br>
+                    <h3>Menu 1</h3>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row" style="margin-top: 50px;"></div>
+    <div class="row">
         <div class="col-md-6 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
