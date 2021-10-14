c9a2e980816f52a64f1260d5094bfdb14551cfcf
IPKIN9 <irwandirini@gmail.con>
2021-10-14 15:55:00 +0800

    Create auth controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('Auth.Login');
+    }
+
+    public function check(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect(route('dashboard.index'));
+        }
+
+        return back()->with('status', 'Username dan Password tidak terdaftar !');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Cms\GolonganController;
 use App\Http\Controllers\Cms\KaryawanController;
 use App\Http\Controllers\Cms\PendaftaranController;
@@ -14,78 +15,82 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
+Route::prefix('auth')->group(function () {
+    Route::get('login', [AuthController::class, 'index'])->name('login');
+    Route::post('check', [AuthController::class, 'check'])->name('auth.check');
+});
+
 Route::prefix('/home')->group(function () {
     Route::post('insertpermintaan', [HomeController::class, 'permintaan'])->name('permintaan.insert');
     Route::post('insertpengaduan', [HomeController::class, 'pengaduan'])->name('pengaduan.insert');
 });
 
-Route::get('Dashboard', function () {
-    return view('Cms.Dashboard');
-});
-Route::get('/contoh', function () {
-    return view('Contoh.Contoh');
-})->name('contoh.index');
+Route::middleware('auth')->group(function () {
+    Route::get('Dashboard', function () {
+        return view('Cms.Dashboard');
+    });
 
-Route::prefix('/golongan')->group(function () {
-    Route::get('index', [GolonganController::class, 'index'])->name('golongan.index');
-    Route::post('create', [GolonganController::class, 'create'])->name('golongan.create');
-    Route::get('getSpecData/{id}', [GolonganController::class, 'detail']);
-    Route::post('update', [GolonganController::class, 'update'])->name('golongan.update');
-    Route::delete('deleteDetailData/{id}', [GolonganController::class, 'destroy']);
-    Route::delete('deleteSpecData/{id}', [GolonganController::class, 'delete']);
-});
+    Route::prefix('/golongan')->group(function () {
+        Route::get('index', [GolonganController::class, 'index'])->name('golongan.index');
+        Route::post('create', [GolonganController::class, 'create'])->name('golongan.create');
+        Route::get('getSpecData/{id}', [GolonganController::class, 'detail']);
+        Route::post('update', [GolonganController::class, 'update'])->name('golongan.update');
+        Route::delete('deleteDetailData/{id}', [GolonganController::class, 'destroy']);
+        Route::delete('deleteSpecData/{id}', [GolonganController::class, 'delete']);
+    });
 
-Route::prefix('/daftar')->group(function () {
-    Route::get('index', [PendaftaranController::class, 'index'])->name('daftar.index');
-    Route::get('getDetailGolongan/{id}', [PendaftaranController::class, 'detail']);
-    Route::post('create', [PendaftaranController::class, 'create'])->name('daftar.create');
-    Route::get('getSpecData/{id}', [PendaftaranController::class, 'detail_edit']);
-    Route::post('update', [PendaftaranController::class, 'update'])->name('daftar.update');
-    Route::delete('deleteSpecData/{id}', [PendaftaranController::class, 'delete']);
-    Route::get('printDataPernyataan/{id}', [PendaftaranController::class, 'cetak']);
-});
+    Route::prefix('/daftar')->group(function () {
+        Route::get('index', [PendaftaranController::class, 'index'])->name('daftar.index');
+        Route::get('getDetailGolongan/{id}', [PendaftaranController::class, 'detail']);
+        Route::post('create', [PendaftaranController::class, 'create'])->name('daftar.create');
+        Route::get('getSpecData/{id}', [PendaftaranController::class, 'detail_edit']);
+        Route::post('update', [PendaftaranController::class, 'update'])->name('daftar.update');
+        Route::delete('deleteSpecData/{id}', [PendaftaranController::class, 'delete']);
+        Route::get('printDataPernyataan/{id}', [PendaftaranController::class, 'cetak']);
+    });
 
-Route::prefix('/karyawan')->group(function () {
-    Route::get('index', [KaryawanController::class, 'index'])->name('karyawan.index');
-    Route::post('insert', [KaryawanController::class, 'insert'])->name('karyawan.insert');
-    Route::get('getspecdata/{id}', [KaryawanController::class, 'edit']);
-    Route::post('update', [KaryawanController::class, 'update'])->name('karyawan.update');
-    Route::delete('deletespecdata/{id}', [KaryawanController::class, 'delete']);
-});
-Route::prefix('/user')->group(function () {
-    Route::get('index', [UserController::class, 'index'])->name('user.index');
-    Route::post('insert', [UserController::class, 'insert'])->name('user.insert');
-    Route::get('getspecdata/{id}', [UserController::class, 'edit']);
-    Route::post('update', [UserController::class, 'update'])->name('user.update');
-    Route::delete('deletespecdata/{id}', [UserController::class, 'delete']);
-});
+    Route::prefix('/karyawan')->group(function () {
+        Route::get('index', [KaryawanController::class, 'index'])->name('karyawan.index');
+        Route::post('insert', [KaryawanController::class, 'insert'])->name('karyawan.insert');
+        Route::get('getspecdata/{id}', [KaryawanController::class, 'edit']);
+        Route::post('update', [KaryawanController::class, 'update'])->name('karyawan.update');
+        Route::delete('deletespecdata/{id}', [KaryawanController::class, 'delete']);
+    });
+    Route::prefix('/user')->group(function () {
+        Route::get('index', [UserController::class, 'index'])->name('user.index');
+        Route::post('insert', [UserController::class, 'insert'])->name('user.insert');
+        Route::get('getspecdata/{id}', [UserController::class, 'edit']);
+        Route::post('update', [UserController::class, 'update'])->name('user.update');
+        Route::delete('deletespecdata/{id}', [UserController::class, 'delete']);
+    });
 
-Route::group(['prefix' => 'dashboard'], function () {
-    Route::get('index', [DashController::class, 'index'])->name('dashboard.index');
-});
+    Route::group(['prefix' => 'dashboard'], function () {
+        Route::get('index', [DashController::class, 'index'])->name('dashboard.index');
+    });
 
-Route::prefix('/pemakaian')->group(function () {
-    Route::get('index', [PemakaianController::class, 'index'])->name('pemakaian.index');
-    Route::post('insert', [PemakaianController::class, 'insert'])->name('pemakaian.insert');
-    Route::get('getspecdata/{id}', [PemakaianController::class, 'edit']);
-    Route::post('update', [PemakaianController::class, 'update'])->name('pemakaian.update');
-    Route::delete('deletespecdata/{id}', [PemakaianController::class, 'delete']);
-});
+    Route::prefix('/pemakaian')->group(function () {
+        Route::get('index', [PemakaianController::class, 'index'])->name('pemakaian.index');
+        Route::post('insert', [PemakaianController::class, 'insert'])->name('pemakaian.insert');
+        Route::get('getspecdata/{id}', [PemakaianController::class, 'edit']);
+        Route::post('update', [PemakaianController::class, 'update'])->name('pemakaian.update');
+        Route::delete('deletespecdata/{id}', [PemakaianController::class, 'delete']);
+    });
 
-Route::prefix('tentang_kami')->group(function () {
-    Route::get('index', [TentangKamiController::class, 'index'])->name('tentangkami.index');
-    Route::post('create', [TentangKamiController::class, 'create'])->name('tentangkami.create');
-});
-Route::prefix('/home')->group(function () {
-    Route::get('index', [HomesController::class, 'permintaanindex'])->name('permintaan.index');
-    Route::delete('deletespecdata/{id}', [HomesController::class, 'deletepermintaan']);
-    Route::get('pengaduan', [HomesController::class, 'pengaduanindex'])->name('pengaduan.index');
-    Route::delete('delete/{id}', [HomesController::class, 'deletepengaduan']);
-});
+    Route::prefix('tentang_kami')->group(function () {
+        Route::get('index', [TentangKamiController::class, 'index'])->name('tentangkami.index');
+        Route::post('create', [TentangKamiController::class, 'create'])->name('tentangkami.create');
+    });
+    Route::prefix('/home')->group(function () {
+        Route::get('index', [HomesController::class, 'permintaanindex'])->name('permintaan.index');
+        Route::delete('deletespecdata/{id}', [HomesController::class, 'deletepermintaan']);
+        Route::get('pengaduan', [HomesController::class, 'pengaduanindex'])->name('pengaduan.index');
+        Route::delete('delete/{id}', [HomesController::class, 'deletepengaduan']);
+    });
 
-Route::prefix('pembayaran')->group(function () {
-    Route::get('index', [PembayaranController::class, 'index'])->name('pembayaran.index');
-    Route::get('getDataStatus/{id}', [PembayaranController::class, 'status']);
-    Route::post('create', [PembayaranController::class, 'create'])->name('pembayaran.insert');
-    Route::get('printData/{id}', [PembayaranController::class, 'cetak']);
+    Route::prefix('pembayaran')->group(function () {
+        Route::get('index', [PembayaranController::class, 'index'])->name('pembayaran.index');
+        Route::get('getDataStatus/{id}', [PembayaranController::class, 'status']);
+        Route::post('create', [PembayaranController::class, 'create'])->name('pembayaran.insert');
+        Route::get('printData/{id}', [PembayaranController::class, 'cetak']);
+    });
 });
